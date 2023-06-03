dac8395b9e882049dbbe08788a5a6de74f0d1f36
Sharuda <sharuda.yana98@gmail.com>
2023-06-03 18:20:03 +0300

    add files with redux

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,32 @@
+import { useSelector } from 'react-redux';
 
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
-export function App() {
- 
+import { Layout } from './Layout/Layout';
+import { Section } from './Section/Section';
+import { Title } from './Title/Title';
+import { Filter } from './Filter/Filter';
+import { ContactForm } from './ContactForm/ContactForm';
+import { ContactList } from './ContactList/ContactList';
 
-  return <div></div>;
-}
+import { getContacts } from 'redux/contacts/contacts-selectors';
+
+export const App = () => {
+  const contacts = useSelector(getContacts);
+  return (
+    <Layout>
+      <Section title="PhoneBook">
+        <ContactForm />
+        {contacts.length > 0 && (
+          <>
+            <Title title="Contacts" />
+            <Filter />
+            <ContactList />
+          </>
+        )}
+      </Section>
+      <ToastContainer />
+    </Layout>
+  );
+};
